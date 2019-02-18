74df8fcb0305e26b0604851c10275d2792c35dea
Burak Targaç <burak.targac@sahabt.com>
2019-02-18 10:18:45 +0300

    fixes unmatched portfolio error caching in memcachier

--- middlewares/cache.middleware.js
+++ middlewares/cache.middleware.js
@@ -1,7 +1,7 @@
 // cache middleware for rendered views
 
 const cacheViewFactory = (mc) => {
-    const cacheView = (req, res, next) => {
+    return (req, res, next) => {
         const view_key = '_view_cache_' + req.originalUrl || req.url;
 
         mc.get(view_key, function(err, val) {
@@ -11,19 +11,20 @@ const cacheViewFactory = (mc) => {
             }
             // Cache the rendered view for future requests
             res.sendRes = res.send;
-            res.send = function(body){
-                mc.set(view_key, body, {expires: 0}, function(err, val){
-                    if(err) {
-                        console.log('err occurred while caching the rendered portfolio view', err);
-                    }
-                });
+            res.send = function (body) {
+                if (typeof body === 'string') {
+                    mc.set(view_key, body, {expires: 0}, function (err, val) {
+                        if (err) {
+                            console.log('err occurred while caching the rendered portfolio view', err);
+                        }
+                    });
+                }
                 res.sendRes(body);
             };
             next();
         });
     };
 
-    return cacheView;
 };
 
 
